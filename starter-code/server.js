25ee3045559b11a42fddb4a0eac9f67a1f297b2b
Tyler W <tyler.walser@yahoo.com>
2017-07-19 16:56:41 -0700

    completed the connection string/set constring to connect to local postgres database

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// TODO: Install and require the NPM Postgres package 'pg' into your server.js, and ensure that it is then listed as a dependency in your package.json
+// TODO: DONE Install and require the NPM Postgres package 'pg' into your server.js, and ensure that it is then listed as a dependency in your package.json
 const fs = require('fs');
 const express = require('express');
 const pg = require('pg');
@@ -13,9 +13,9 @@ const app = express();
 // TODO: Complete the connection string for the url that will connect to your local postgres database
 // Windows and Linux users; You should have retained the user/pw from the pre-work for this course.
     // Your url may require that it's composed of additional information including user and password
-    // const conString = 'postgres://USER:PASSWORD@HOST:PORT/DBNAME';
+const conString = `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432`;
     // make sure you save that password as an environment variable, and read it in using process.env (like how we read process.env.PORT above). NEVER commit a password, token, or any form of credential in your code. That is what environment variables are for.
-const conString = 'postgres://localhost:5432';
+// const conString = 'postgres://localhost:5432';
 
 // TODO: Our pg module has a Client constructor that accepts one argument: the conString we just defined.
 //       This is how it knows the URL and, for Windows and Linux users, our username and password for our
